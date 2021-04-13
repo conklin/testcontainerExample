b871283d434c6b4bc790ae6a8706b42bffd3e499
mconklin <ssi.conklin@gmail.com>
2021-04-12 20:46:47 -0400

    init commit

---
+++ src/main/java/dev/mconklin/testcontainerExample/TestcontainerExampleApplication.java
@@ -0,0 +1,13 @@
+package dev.mconklin.testcontainerExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestcontainerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestcontainerExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/mconklin/testcontainerExample/TestcontainerExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mconklin.testcontainerExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainerExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
