1982cd5a0d16a652a7d3d4a9f12ba544b86ce576
mconklin <ssi.conklin@gmail.com>
2021-04-12 21:16:50 -0400

    add test container test

--- src/test/java/dev/mconklin/testcontainerExample/TestcontainerExampleApplicationTests.java
+++ src/test/java/dev/mconklin/testcontainerExample/TestcontainerExampleApplicationTests.java
@@ -1,13 +1,29 @@
 package dev.mconklin.testcontainerExample;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+import redis.clients.jedis.Jedis;
 
 @SpringBootTest
+@Testcontainers
 class TestcontainerExampleApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
+    @Container
+    public GenericContainer redis
+            = new GenericContainer(DockerImageName.parse("redis:5.0.3-alpine")).withExposedPorts(6379);
+
+
+    @Test
+    void contextLoads() {
+        Jedis jedis = new Jedis(redis.getContainerIpAddress(), redis.getMappedPort(6379));
+        jedis.append("foo", "bar");
+        System.out.println(jedis.get("foo"));
+    }
 
 }
